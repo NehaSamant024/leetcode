69bdd01aabe1da8b9cceb95869eee2c3169797f7
Neha Samant <91238040+NehaSamant024@users.noreply.github.com>
2023-03-09 17:48:10 +0530

    Time: 7 ms (58.02%), Space: 6 MB (57.70%) - LeetHub

---
+++ 0441-arranging-coins/0441-arranging-coins.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int arrangeCoins(int n) {
+        int count=0;
+        if(n==1)
+            return 1;
+       for(int i=1;i<=n;i++)
+       {
+           if(i<=n)
+           {
+               n=n-i;
+               count++;
+
+          
+           }
+       }
+        return count;
+    }
+};
