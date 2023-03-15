7a40a0d627e5b52a3206b1c0cef689850eef91ae
Neha Samant <91238040+NehaSamant024@users.noreply.github.com>
2023-03-15 22:47:50 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (84.69%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.cpp
+++ 1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -1,3 +1,4 @@
+/*
 class Solution {
 public:
     int lastStoneWeight(vector<int>& stones) {
@@ -14,5 +15,30 @@ public:
         //  8-7=1 // pq=[1,1,1,2,4]
         }
         return pq.top();
+    }
+};
+*/
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int>pq (stones.begin(),stones.end());
+        while(pq.size()>=2)
+        {
+            int y=pq.top();
+            pq.pop();
+            int x=pq.top();
+            pq.pop();
+            if(x==y)
+            {
+                
+            }
+            else
+            {
+                pq.push(y-x);
+            }
+        }
+        if(!pq.empty()) return pq.top();
+        return 0;
+        
     }
 };
