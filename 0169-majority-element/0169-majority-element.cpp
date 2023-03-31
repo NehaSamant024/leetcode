cf4f659d8ed6af78b9ac936b849c5f441799a9a2
Neha Samant <91238040+NehaSamant024@users.noreply.github.com>
2023-03-31 22:59:01 +0530

    Time: 24 ms (39.85%), Space: 19.7 MB (38.41%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.cpp
@@ -0,0 +1,32 @@
+/*class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+      int n=nums.size();
+        int count=0;
+        int max=nums[0];
+        for(int i=0;i<n-1;i++){
+          //  int max=nums[0];
+            if(nums[i]>max)
+            {
+                max=nums[i];
+            }
+                          //  count++;
+
+https://leetcode.com/problems/majority-element/solution/        }
+        return max;
+    }
+};
+*/
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        
+      //  int k=nums.size()/2;
+        int n=nums.size();
+        
+        sort(nums.begin(),nums.end());
+        
+        return nums[n/2];
+        
+    }
+};
