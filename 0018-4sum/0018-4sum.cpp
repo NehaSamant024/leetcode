3bc1f26ac737c2346355338f9e01f4bc265b08cb
Neha Samant <91238040+NehaSamant024@users.noreply.github.com>
2023-05-05 16:47:23 +0530

    Time: 441 ms (14.62%), Space: 49.3 MB (28.50%) - LeetHub

---
+++ 0018-4sum/0018-4sum.cpp
@@ -0,0 +1,69 @@
+/*class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        set<vector<int>> set;
+        vector<vector<int>> output;
+        for(int i=0; i<n-3; i++){
+            for(int j=i+1; j<n-2; j++){
+                for(int k=j+1; k<n-1; k++){
+                    for(int l=k+1; l<n; l++){
+                        if((long long)nums[i] + (long long)nums[j] + (long long)nums[k] + 
+                        (long long)nums[l] == target){
+                            set.insert({nums[i], nums[j], nums[k], nums[l]});
+                        }
+                    }
+                }
+            }
+        }
+        for(auto it : set){
+            output.push_back(it);
+        }
+        return output;
+    }
+};
+time limit exceed
+*/
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+           set<vector<int>> s;
+        vector<vector<int>> ans;
+        
+        if (nums.size()<4) {
+            return ans;
+        }
+        
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        
+        for (int i=0; i<n-3; i++) {
+            for (int j=i+1; j<n-2; j++) {
+                int low = j+1;
+                int high = n-1;
+                
+                while (low<high) {
+                  long long sum = (long long)nums[i] + nums[j] + nums[low] + nums[high];                    
+                    if (sum == target) {
+                        s.insert({nums[i], nums[j], nums[low], nums[high]});
+                        low++;
+                        high--;
+                    }
+                    else if (sum<target) {
+                        low++;
+                    }
+                    else {
+                        high--;
+                    }
+                }
+            }
+        }
+        
+        for (auto x : s) {
+            ans.push_back(x);
+        }
+        
+        return ans;
+    }
+};
